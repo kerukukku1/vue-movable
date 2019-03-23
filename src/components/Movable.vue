13bc13ebb72bbda8da0254f14e03f2eae8e4f36c
Mahito Fujino <kerukukku1@gmail.com>
2019-03-23 18:52:53 +0900

    remove app.vue and create movable, drag_component, resizable_frame

---
+++ src/components/Movable.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="movable">
+        movable container
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.movable {
+
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Movable from './components/Movable.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(Movable)
 }).$mount('#app')
