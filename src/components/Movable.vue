d976ee67c5e96aa1da3220266c99083b43dc1c28
Mahito Fujino <kerukukku1@gmail.com>
2019-03-24 15:07:19 +0900

    directiveからイベントを発行できるよう実装

--- src/components/Movable.vue
+++ src/components/Movable.vue
@@ -1,16 +1,46 @@
 <template>
-    <div class="movable">
-        movable container
-    </div>
+  <div
+    class="movable"
+    :style="style"
+    :width="width"
+    :height="height"
+    v-movable
+  >
+    <slot></slot>
+  </div>
 </template>
 
 <script>
 export default {
+  props: {
+    width: {
+      types: Number,
+      default: 400
+    },
+    height: {
+      types: Number,
+      default: 400
+    }
+  },
+  computed: {
+    style: function () {
+      return {
+        width: this.width + 'px',
+        height: this.height + 'px'
+      }
+    }
+  },
+  methods: {
+    test: function () {
+      console.log('test')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .movable {
-
+  position: relative;
+  border: 1px solid black;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
-import Movable from './components/Movable.vue'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.config.productionTip = false
 
+Vue.directive('movable', {
+  bind: function (el, binding, vnode) {
+    el.addEventListener('mouseleave', function () {
+      vnode.context.$emit('test')
+    })
+  }
+})
+
 new Vue({
-  render: h => h(Movable)
+  render: h => h(HelloWorld)
 }).$mount('#app')
